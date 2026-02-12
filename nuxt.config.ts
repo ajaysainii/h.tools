9832f9868d81f221461524ada520913d5619ca7d
ajay <ajay.s@sigmainfo.net>
2026-02-12 23:35:22 +0530

    Redirected index to /text/conver

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: true,
+  routeRules: {
+    '/': { redirect: '/text/convert' },
+  },
   css: [
     '@/assets/css/vendors.css',
     '@/assets/css/main.css',
